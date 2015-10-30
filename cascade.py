28c88ba582a5c28a7b4f41b26003fae8f84f3596
Jordan <jordanespejo@csus.edu>
2015-10-30 12:02:29 -0700

    Create cascade.py

---
+++ cascade.py
@@ -0,0 +1,49 @@
+filename = open("output.txt")            
+outputfile = open("final.txt","w+")
+
+trips = []
+
+for line in filename:
+
+	if line.startswith("trip"):
+		continue
+	
+        trip = line.split(',')
+        trips.append(trip)
+pos = 0
+tripread = 0
+tripfile = 0
+filename = open("output.txt")         
+
+for line in filename:
+        trip = line.split(',')
+
+	if line.startswith("trip"):
+		continue
+
+	tripread = int(trip[4])
+	tripprev = tripread - 1
+
+	tripfile = int(trips[pos-1][4])
+	print "this",tripread,"prev",tripfile,line
+	if tripread == tripfile + 2:
+		print "^^this replaced with vv"
+		line = line.replace(","+trip[4]+",",","+str(tripprev)+",")
+		print line
+		print "this seq",trip[4],"prev seq",trips[pos-1][4]
+		flag = 1
+		outputfile.write(line)
+		pos += 1
+		continue
+
+	if flag:
+
+		if tripread == tripfile+1:
+			print line
+			line = line.replace(","+trip[4]+",",","+str(tripfile)+",")
+			print "replaced with",line
+		else:
+			flag = None
+
+	outputfile.write(line)
+	pos += 1
