5fbb81091d7f6e87e11c60b1f2fc9746616d2de6
Jordan <jespejoo@yahoo.com>
2016-03-21 11:03:18 -0700

    fixed having to manual
    kept headers, last few lines for fix

--- source/cascade.py
+++ source/cascade.py
@@ -21,18 +21,15 @@ for line in filename:
         trip = line.split(',')
 
 	if line.startswith("trip"):
+		outputfile.write(line)
 		continue
 
 	tripread = int(trip[4])
 	tripprev = tripread - 1
 
 	tripfile = int(trips[pos-1][4])
-	print "this",tripread,"prev",tripfile,line
 	if tripread == tripfile + 2:
-		print "^^this replaced with vv"
 		line = line.replace(","+trip[4]+",",","+str(tripprev)+",")
-		print line
-		print "this seq",trip[4],"prev seq",trips[pos-1][4]
 		flag = 1
 		outputfile.write(line)
 		pos += 1
@@ -41,9 +38,7 @@ for line in filename:
 	if flag:
 
 		if tripread == tripfile+1:
-			print line
 			line = line.replace(","+trip[4]+",",","+str(tripfile)+",")
-			print "replaced with",line
 		else:
 			flag = None
 
@@ -51,3 +46,4 @@ for line in filename:
 	pos += 1
 filename.close()
 os.remove('output.txt')
+print "gtfs final.txt created"
--- source/fix.py
+++ source/fix.py
@@ -1,5 +1,5 @@
 filename = raw_input("Enter file name (stop_times.txt): ")
-if len(filename) < 1 : filename ="input.txt"
+if len(filename) < 1 : filename ="stop_times.txt"
 
 inputfile = open(filename)                    
 outputfile = open("output.txt","w+")            
@@ -19,7 +19,8 @@ inputfile = open(filename)
 pos=0
 for line in inputfile:
         if line.startswith("trip"):
-                continue
+        	outputfile.write(line)
+        	continue
 
 
 	trip = line.split(',')
@@ -28,25 +29,17 @@ for line in inputfile:
 	arr = trip[1]
 	seq = int(trip[4])
 
-	if id == trips[pos+1][3] and seq+1 == int(trips[pos+1][4]):
-		print "\tTIME CHANGED",id
-		line = line.replace(dep,trips[pos+1][2])
-		line = line.replace(trips[pos+1][2],arr,1)
-		
-	pos += 1
+	try:
+		if id == trips[pos+1][3] and seq+1 == int(trips[pos+1][4]):
+			line = line.replace(dep,trips[pos+1][2])
+			line = line.replace(trips[pos+1][2],arr,1)
+
+		pos += 1
 	
-	print "\tseq:",seq,"\ttripseq",trips[pos][4]
+		if id == trips[pos-2][3] and trip[0] == trips[pos-2][0]:
+			continue	
+	except:
+		pass
 
-	print trip[0],trips[pos][0]
-	if id == trips[pos-2][3] and trip[0] == trips[pos-2][0]:
-		print id,"\tREMOVED"," trip id:",trips[pos-2][3]
-		continue	
-
-	outputfile.write(line)	
-
-
-	if pos == len(trips) - 2:
-		break
-
-filename = open("output.txt")          
-
+	outputfile.write(line)        
+print "gtfs output.txt created"
